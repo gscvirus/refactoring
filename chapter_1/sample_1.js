6e491a208640ac7ea7713bac2791286f21d58538
GSCVirus <gonzalo.solano@live.com>
2018-12-25 21:38:22 -0400

    Initial commit without refactoring

---
+++ chapter_1/sample_1.js
@@ -0,0 +1,70 @@
+function generateStatement(plays, invoice) {
+    let totalAmount = 0;
+    let volumeCredits = 0;
+    let result = `Statement for ${invoice.customer}\n`;
+    const format = new Intl.NumberFormat("en-US"
+                                        , { style: "currency",
+                                            currency: "USD",
+                                            minimumFractionDigits: 2}).format;
+
+    for (let perf of invoice.performances) {
+        const play = plays[perf.playID];
+        let thisAmount = 0;
+
+        switch (play.type) {
+            case "tragedy":
+                thisAmount = 40000;
+                if (perf.audience > 30) {
+                    thisAmount += 1000 * (perf.audience - 30);
+                }
+                break;
+            case "comedy":
+                thisAmount = 30000;
+                if (perf.audience > 20) {
+                    thisAmount += 10000 + 500 * (perf.audience - 20);
+                }
+                thisAmount += 300 * perf.audience;
+                break;
+            default:
+                throw new Error(`unknown type: ${play.type}`);
+        }
+
+        volumeCredits += Math.max(perf.audience - 30, 0);
+        if ("comedy" === play.type) {
+            volumeCredits += Math.floor(perf.audience / 5);
+        }
+
+        result += `  ${play.name}: ${format(thisAmount / 1000)} (${perf.audience} seats)\n`;
+        totalAmount += thisAmount;
+    }
+
+    result += `Amount owed is ${format(totalAmount / 100)}\n`;
+    result += `You earned ${volumeCredits} credits\n`;
+
+    return result;
+}
+
+var result = generateStatement({
+        "hamlet": {"name": "Hamlet", "type": "tragedy"},
+        "as-like": {"name": "As you like it", "type": "comedy"},
+        "othello": {"name": "Othello", "type": "tragedy"}
+    },
+    {
+        "customer": "BigCo",
+        "performances": [
+            { 
+                "playID": "hamlet",
+                "audience": 55
+            },
+            {
+                "playID": "as-like",
+                "audience": 35
+            },
+            {
+                "playID": "othello",
+                "audience": 40
+            }
+        ]
+    });
+
+console.log(result);
